2c24161a2028ede7e300e77b562c72678525c165
eric <phantomenace87@gmail.com>
2025-02-15 13:41:36 -0800

    start of new assignment that adds color

--- src/Line/ScanConvertAbstract.java
+++ src/Line/ScanConvertAbstract.java
@@ -1,5 +1,7 @@
 package Line;
 
+import Color.*;
+
 public abstract class ScanConvertAbstract {
 	/**
 	 * Scan convert a line segment using the Digital Differential Analyzer algorithm
@@ -12,17 +14,20 @@ public abstract class ScanConvertAbstract {
 	 * @throws ArrayIndexOutOfBoundsException thrown if a coordinate goes out of range of the frame buffer
 	 */
 	public abstract void digitaldifferentialanalyzer(int x0, int y0, int x1, int y1, int framebuffer[][][]) throws NullPointerException, ArrayIndexOutOfBoundsException;
-	
+		
 	/**
-	 * Scan convert a line segment using Bresenham's algorithm
+	 * Scan convert a line segment using Bresenham's algorithm 
+	 * with start to end color interpolation
 	 * @param x0 starting x coordinate
 	 * @param y0 starting y coordinate
 	 * @param x1 ending x coordinate
 	 * @param y1 ending y coordinate
+	 * @param c0 starting color
+	 * @param c1 ending color
 	 * @param framebuffer frame buffer to receive the scan conversion
 	 * @throws NullPointerException thrown if the frame buffer is null
 	 * @throws ArrayIndexOutOfBoundsException thrown if a coordinate goes out of range of the frame buffer
-	 */
-	public abstract void bresenham(int x0, int y0, int x1, int y1, int framebuffer[][][])  throws NullPointerException, ArrayIndexOutOfBoundsException;
+	 */	public abstract void bresenham(int x0, int y0, int x1, int y1, ColorAbstract c0, ColorAbstract c1, int framebuffer[][][])  throws NullPointerException, ArrayIndexOutOfBoundsException;
+
 
 }
---
+++ src/Line/ScanConvertColorTest.java
@@ -0,0 +1,30 @@
+package Line;
+
+import Common.ReadWriteImage;
+import Color.*;
+
+public class ScanConvertColorTest {
+	public static void main (String[] args) {
+		ScanConvertAbstract sc = new ScanConvertLine();
+		ColorAbstract c0 = new Color(1.0, 0.0, 0.0);
+		ColorAbstract c1 = new Color(0.0, 1.0, 0.0);
+		int anglesteps = 32;
+
+		{
+			int framebuffer[][][] = new int[3][256][256];
+			try {
+				for (int x = 0; x < framebuffer[0][0].length; x += anglesteps) {
+					sc.bresenham(x, 0, framebuffer[0][0].length - x - 1, framebuffer[0].length - 1, c0, c1, framebuffer);
+				}
+				for (int y = 0; y < framebuffer[0].length; y += anglesteps) {
+					sc.bresenham(0, y, framebuffer[0][0].length - 1, framebuffer[0].length - y - 1, c0, c1, framebuffer);
+				}
+				ReadWriteImage.writeImage(framebuffer, "bresenhamcolor.png");
+			}
+			catch (Exception e) {
+				System.out.println(e);
+			}
+		}	
+	}
+
+}
