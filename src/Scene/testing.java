5789ab1c67bc8244e5cc8b5a02569212005b4407
eric <phantomenace87@gmail.com>
2025-03-26 11:13:26 -0700

    scene objects + quick test to make sure parser works

---
+++ src/Scene/testing.java
@@ -0,0 +1,9 @@
+package Scene;
+
+public class testing {
+    public static void main(String[] args) {
+        SceneObject s = new SceneObject("Graphics/res/cube-ascii.stl");
+        
+        
+    }
+}
